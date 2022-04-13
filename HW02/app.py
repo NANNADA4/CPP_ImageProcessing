8138f2e22802bfc79cfcd309cb36128c47dd6ba9
NANNADA4 <nannada4@icloud.com>
2022-04-14 06:04:46 +0900

    finish equalization beta

--- HW02/app.py
+++ HW02/app.py
@@ -1,28 +1,44 @@
 import base64
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from io import BytesIO, StringIO
+from datetime import datetime
+from functools import update_wrapper, wraps
+from io import BytesIO
 import matplotlib.pyplot as plt
 from PIL import Image
-from flask import Flask, Response, render_template, request, send_file
+from flask import Flask, make_response, render_template, request
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
+
+
+def nocache(view):
+    @wraps(view)
+    def no_cache(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Last-Modified'] = datetime.now()
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
+    return update_wrapper(no_cache, view)
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@nocache
 def index():
     return render_template('entry.html')
 
 
 @app.route('/main')
+@nocache
 def info():
     return render_template('main.html')
 
 
 @app.route("/server", methods=["POST"])
+@nocache
 def server():
     option = request.form['ImageProcess']
     if (option == 'Histogram'):
@@ -46,19 +62,40 @@ def histogram():
     for h in range(0, height):
         for w in range(0, width):
             Pixel = image.getpixel((w, h))
-            histoArray[Pixel] = histoArray[Pixel] + 1
+            histoArray[Pixel] += 1
 
     plt.plot(histoArray)
     img = BytesIO()
     plt.savefig(img, format='png', dpi=200)
     img.seek(0)
     plot_url = base64.b64encode(img.getvalue()).decode('utf8')
-    # return send_file(img, mimetype='image/png')
+
     return render_template('showImg.html', plot_url=plot_url)
 
 
 def equalization():
-    return 'hello, equlization'
+    image = Image.open('./static/SavedImages/lena_original.bmp')
+
+    width = image.width
+    height = image.height
+
+    histoArray = np.zeros(255, dtype=int)
+    comulativeArray = np.zeros(255, dtype=int)
+
+    for h in range(0, height):
+        for w in range(0, width):
+            Pixel = image.getpixel((w, h))
+            histoArray[Pixel] += 1
+
+    comulativeArray = np.multiply(histoArray, 255 / (512 * 512)).astype('int')
+
+    plt.plot(comulativeArray)
+    img = BytesIO()
+    plt.savefig(img, format='png', dpi=200)
+    img.seek(0)
+    plot_url1 = base64.b64encode(img.getvalue()).decode('utf8')
+
+    return render_template('showImg1.html', plot_url1=plot_url1)
 
 
 def basic():
