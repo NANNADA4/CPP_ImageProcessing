261706b9895b85801bcb2dcbe2f266755f70dbba
NANNADA4 <nannada4@icloud.com>
2022-04-14 04:09:40 +0900

    :sparkles: Add Print histogram

--- HW02/app.py
+++ HW02/app.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, request, send_file
-from werkzeug.utils import secure_filename
-import numpy as np
+import base64
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from io import BytesIO, StringIO
 import matplotlib.pyplot as plt
 from PIL import Image
+from flask import Flask, Response, render_template, request, send_file
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 
 app = Flask(__name__)
 
@@ -23,7 +28,7 @@ def server():
     if (option == 'Histogram'):
         return histogram()
     elif (option == 'Equlization'):
-        return equlization()
+        return equalization()
     elif (option == 'BasicContrast'):
         return basic()
     else:
@@ -31,12 +36,28 @@ def server():
 
 
 def histogram():
-    img = Image.open('./static/SavedImages/lena_original.bmp')
+    image = Image.open('./static/SavedImages/lena_original.bmp')
 
-    return render_template('showImg.html')
+    width = image.width
+    height = image.height
+
+    histoArray = np.zeros(255, dtype=int)
+
+    for h in range(0, height):
+        for w in range(0, width):
+            Pixel = image.getpixel((w, h))
+            histoArray[Pixel] = histoArray[Pixel] + 1
+
+    plt.plot(histoArray)
+    img = BytesIO()
+    plt.savefig(img, format='png', dpi=200)
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+    # return send_file(img, mimetype='image/png')
+    return render_template('showImg.html', plot_url=plot_url)
 
 
-def equlization():
+def equalization():
     return 'hello, equlization'
 
 
