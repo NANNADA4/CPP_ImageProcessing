5be3087e1d5a1a10e7060dc5a841123783c21cd7
NANNADA4 <nannada4@icloud.com>
2022-05-27 12:15:03 +0900

    Finish HW01

--- HW03/app.py
+++ HW03/app.py
@@ -1,13 +1,49 @@
 import base64
+from cmath import sqrt
 from datetime import datetime
 from functools import update_wrapper, wraps
 from io import BytesIO
+import math
 import matplotlib.pyplot as plt
 from PIL import Image
 from flask import Flask, make_response, render_template, request, flash
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
+
+
+img = Image.open("./static/SavedImages/lena_bmp_512x512_new.bmp").convert('L')
+img = np.array(img)
+
+roberts_1 = np.array([[1, 0],
+                      [0, -1]])
+
+roberts_2 = np.array([[0, -1],
+                      [1, 0]])
+
+sobel_x = np.array([[-1/4, 0, 1/4],
+                    [-2/4, 0, 2/4],
+                    [-1/4, 0, 1/4]])
+
+sobel_y = np.array([[1/4, 2/4, 1/4],
+                    [0, 0, 0],
+                    [-1/4, -2/4, -1/4]])
+
+prewitt_x = np.array([[-1/3, 0, 1/3],
+                      [-1/3, 0, 1/3],
+                      [-1/3, 0, 1/3]])
+
+prewitt_y = np.array([[1/3, 1/3, 1/3],
+                      [0, 0, 0],
+                      [-1/3, -1/3, -1/3]])
+
+stochastic_x = np.array([[0.267, 0.364, 0, -0.364, -0.267],
+                         [0.373, 0.562, 0, -0.562, -0.373],
+                         [0.463, 1.000, 0, -1.000, -0.463],
+                         [0.373, 0.562, 0, -0.562, -0.373],
+                         [0.267, 0.364, 0, -0.364, -0.267]])
+
+stochastic_y = stochastic_x.T
 
 
 def nocache(view):
@@ -55,48 +91,56 @@ def ex02():
 def server():
     option = request.form['edge']
     if(option == 'Default'):
-        return default()
+        flash("올바른 옵션을 선택해주세요")
+        return render_template('ex01.html')
     elif(option == 'Roberts'):
-        return roberts()
+        flash("Roberts Edge Detection을 선택하셨습니다.")
+        return edge_detection(img, roberts_1, roberts_2, threshold=150)
     elif(option == 'Sobel'):
-        return sobel()
+        flash("Sobel Edge Detection을 선택하셨습니다.")
+        return edge_detection(img, sobel_x, sobel_y, threshold=150)
     elif(option == 'Prewitt'):
-        return prewitt()
+        flash("Prewitt Edge Detection을 선택하셨습니다.")
+        return edge_detection(img, prewitt_x, prewitt_y, threshold=150)
     elif(option == 'Stochastic'):
-        return stochastic()
+        flash("Stochastic Edge Detection을 선택하셨습니다.")
+        return edge_detection(img, stochastic_x, stochastic_y, threshold=150)
 
 
-def default():
-    flash("올바른 옵션을 선택해주세요")
-    return render_template('ex01.html')
-
-
-def AddGaussian():
-    return
-
-
-def roberts():
-    img = Image.open("./static/SavedImages/lena_bmp_512x512_new.bmp")
-    img = np.array(img)
-
-    roberts_1 = np.array([[1, 0], [0, -1]])
-    roberts_2 = np.array([[0, 1], [-1, 0]])
+def edge_detection(img, mask1, mask2, threshold):
 
     img_shape = img.shape
 
-    return 0
+    try:
+        if mask1.shape != mask2.shape:
+            raise Exception('마스크의 크기가 서로 다릅니다.')
+        filter_size = mask1.shape
+    except Exception as e:
+        print('예외가 발생했습니다.', e)
 
+    result_shape = tuple(np.array(img_shape)-np.array(filter_size)+1)
 
-def sobel():
-    return
+    result1 = np.zeros(result_shape)
+    result2 = np.zeros(result_shape)
 
+    for h in range(0, result_shape[0]):
+        for w in range(0, result_shape[1]):
+            tmp = img[h:h+filter_size[0], w:w+filter_size[1]]
+            result1[h, w] = np.abs(np.sum(tmp*mask1))
+            result2[h, w] = np.abs(np.sum(tmp*mask2))
 
-def prewitt():
-    return
+    result = result1 + result2
 
+    thr_result = np.zeros(result_shape)
+    thr_result[result > threshold] = 1
 
-def stochastic():
-    return
+    plt.imshow(thr_result, cmap='gray')
+    img = BytesIO()
+    plt.savefig(img, format='png', dpi=300)
+    img.seek(0)
+    img_show = base64.b64encode(img.getvalue()).decode('utf8')
+
+    return render_template('ex01.html', img_show=img_show)
 
 
 if __name__ == "__main__":
